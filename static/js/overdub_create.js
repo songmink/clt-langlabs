547998d8d67ad8839403db247696ff5994c211fc
xhspiderman <xhspiderman@gmail.com>
2014-05-14 14:14:07 -1000

    Added toggle for video upload
    added tabs for video uploading from either an URL or user uploaded video

---
+++ static/js/overdub_create.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+  // Handler for .ready() called.
+  $("#div_id_media").addClass(" tab-pane active ")
+  .find('label').remove();
+
+  $("#div_id_upload_video").addClass(" tab-pane ")
+  .find('label').remove();
+  var newElements = '\
+        <ul class="nav nav-tabs" style="margin-bottom:12px;">\
+          <li class="active"><a href="#div_id_media" data-toggle="tab" style="padding: 5px 8px 0px 5px;"><label>Video URL (e.g. http://youtu.be/DJ9zIuFoQ5o)</a></li></label>\
+          <li><a href="#div_id_upload_video" data-toggle="tab" style="padding: 5px 8px 0px 5px;"><label>Upload a Video</label></a></li>\
+        </ul>\
+        <div id="video_tabs" class="tab-content">\
+        </div>\
+        '
+
+  $( newElements ).insertAfter( "#div_id_instructions" );
+  $("#div_id_media").appendTo($("#video_tabs"));
+  $("#div_id_upload_video").appendTo($("#video_tabs"));
+});
