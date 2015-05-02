4715c92a9aecd405d69a51398468e5e44d83e48e
Kenneth Corea <kcorea@hawaii.edu>
2015-05-01 14:07:47 -1000

    fixed/added question mark tooltips

--- static/js/collection_edit.js
+++ static/js/collection_edit.js
@@ -3,19 +3,17 @@
 $(document).ready(function () {
 
     // tooltips for question marks
-    if ($('#div_id_accesscode')) {
+    if ($('#div_id_accesscode').length != 0) {
         $('#div_id_accesscode .control-label').after(' <small><i class="fa questionMark fa-question" style="cursor:pointer;" data-toggle="tooltip" data-placement="right" title="Enter a 10 character code that students will use to subscribe to this course (e.g. l@ng@bzRoXXX)"></i></small>');
     }
 
-/*
-    if ($('#div_id_is_active')) {
-        $('#div_id_is_active').append(' <small><i class="fa questionMark fa-question" style="cursor:pointer;" data-toggle="tooltip" data-placement="right" title="Check this box to allow students to participate in the course, uncheck this box to only allow instructors to view the course"></i></small>');
+    if ($('#div_id_is_active').length != 0) {
+        $('#div_id_is_active').append(' <small><i class="fa questionMark fa-question" style="cursor:pointer;" data-toggle="tooltip" data-placement="right" title="Students can view and participate in a course only while it is active. On the other hand, students cannot view nor subscribe to an inactive course. For instance, deactivate the course while you populate its content and activate it when it is ready."></i></small>');
     }
 
-    if ($('#div_id_is_public')) {
-        $('#div_id_is_public').append(' <small><i class="fa questionMark fa-question" style="cursor:pointer;" data-toggle="tooltip" data-placement="right" title="wat?"></i></small>');
+    if ($('#div_id_is_public').length != 0) {
+        $('#div_id_is_public').append(' <small><i class="fa questionMark fa-question" style="cursor:pointer;" data-toggle="tooltip" data-placement="right" title="Anyone in Lang Labs can participate in a public course and the subscription code is public. Uncheck this box to hide the subscription code and make the course private."></i></small>');
     }
-*/
     $('.questionMark').mouseenter(function() {
 
         $(this).tooltip('show')
