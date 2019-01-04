81449e61b668e3cb6bf60e7ced6517a70e38f0ca
Songmink <songmink@hawaii.edu>
2019-01-03 15:54:27 -1000

    clean code

---
+++ static/js/base.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	// subscribe to a new course 
+   var subscribeURL  =  $("#courseSubscribeButton").data('subscribeurl')
+   $("#courseSubscribeButton").click(function(event){
+        event.preventDefault();
+        var GOTOURL=subscribeURL.replace('ToReplace',$('#accesscodearea').val() )
+        window.location.href = GOTOURL;
+   })
+
+});
