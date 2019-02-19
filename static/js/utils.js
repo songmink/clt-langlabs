5f3e2e076a50262dbf38dec56ef4689eb48ab17c
Songmink <songmink@hawaii.edu>
2019-02-19 11:44:58 -1000

    cleaned code

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,26 +1,23 @@
 // utils.js
 
 // ajax stuff
-var Ajax = (function() {
-
-    var post = function(ajax_url, data, csrftoken, callback){
+var Ajax = (function () {
+    var post = function (ajax_url, data, csrftoken, callback) {
         $.ajax({
             url: ajax_url,
             data: data,
-            type: "POST",
+            type: 'POST',
             async: false,
-            beforeSend: function(xhr) {
-                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('X-CSRFToken', csrftoken);
             }
         })
-        .done(callback)
-        .fail(function(jqXHR, textStatus) {
-        });
+            .done(callback)
+            .fail(function (jqXHR, textStatus) {});
     };
 
     return {
         post: post
-    }
+    };
 
 })();
-
