849f31b1d066be14a98d5789269f3daffed2a811
Songmink <songmink@hawaii.edu>
2019-04-12 13:37:25 -1000

    clean code

--- static/js/activity_create.js
+++ static/js/activity_create.js
@@ -1,122 +1,230 @@
-$( document ).ready(function() {
-  // Handler for .ready() called.
+$(document).ready(function () {
+    // Handler for .ready() called.
 
-  $("#div_id_lesson").find('label').after('&nbsp;&nbsp;&nbsp;<button id="modalFire" type="button" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-plus"></span>New</button>');
-  
-  // toggle modal using the '+' sign
-  $( "#modalFire" ).click(function() {
-	    $('#myModal').modal('toggle');
-	});
-  
-  $('#myModal').on('hide.bs.modal', function (e) {
-  	// obtain the current URL and use it to refresh the content we want
-  	    var currentURL = window.location.href
-        $("#id_lesson").parent().load(currentURL +  '  #id_lesson')
-	})
-  // fake the rich text editor using another div
-  $("#id_instructions").addClass("hidden"); 
-  $( "#id_instructions" ).after( '<div id="InstructionTextArea" class="form-control" style="border-radius:4px 4px 4px 4px;"></div>' );
-  
-  // initialize the text edior
-  rteInit()
-  $( "#InstructionTextArea" ).append( $("#id_instructions").val());
-  $( "form" ).submit(function( event ) {
-      $( "#id_instructions" ).val($("#InstructionTextArea").html())
+    $("#div_id_lesson").find('label').after('&nbsp;&nbsp;&nbsp;<button id="modalFire" type="button" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-plus"></span>New</button>');
+
+    // toggle modal using the '+' sign
+    $("#modalFire").click(function () {
+        $('#myModal').modal('toggle');
     });
 
-  // Add explaination to private mode
-  if($('#div_id_private_mode').length==1){
-    $('#div_id_private_mode').append(' <small><i class="fa questionMark fa-question" style="cursor:pointer;" data-toggle="tooltip" data-placement="right" title="In Private mode student only interact with instructors and their posts will not be viewable by other students."></i></small>')
-    
-  }
+    $('#myModal').on('hide.bs.modal', function (e) {
+        // obtain the current URL and use it to refresh the content we want
+        var currentURL = window.location.href
+        $("#id_lesson").parent().load(currentURL + '    #id_lesson')
+    })
+    // fake the rich text editor using another div
+    $("#id_instructions").addClass("hidden");
+    $("#id_instructions").after('<div id="InstructionTextArea" class="form-control" style="border-radius:4px 4px 4px 4px;"></div>');
 
-  // Add explaination to read_after_post
-  if($('#div_id_read_after_post').length==1){
-    $('#div_id_read_after_post').append(' <small><i class="fa questionMark fa-question " style="cursor:pointer;" data-toggle="tooltip" data-placement="right" title="In Read After Post mode a student cannot view other students\' posts until they have published a post of their own."></i></small>')
-  }
+    // initialize the text edior
+    rteInit()
+    $("#InstructionTextArea").append($("#id_instructions").val());
+    $("form").submit(function (event) {
+        $("#id_instructions").val($("#InstructionTextArea").html())
+    });
 
-  // Add explaination to read_after_post
-  if($('#div_id_is_active').length==1){
-    $('#div_id_is_active').append(' <small><i class="fa questionMark fa-question " style="cursor:pointer;" data-toggle="tooltip" data-placement="right" title="Students can participate in the activity only while it is active. On the other hand, students cannot access an inactive activity and it will not appear in their activity list. For example, deactivate an activity after an assignment deadline or while you are populating the activity content."></i></small>')
-  }
+    // Add explaination to private mode
+    if ($('#div_id_private_mode').length == 1) {
+        $('#div_id_private_mode').append(' <small><i class="fa questionMark fa-question" style="cursor:pointer;" data-toggle="tooltip" data-placement="right" title="In Private mode student only interact with instructors and their posts will not be viewable by other students."></i></small>')
 
-  $('.questionMark').mouseenter(function(e) {
+    }
 
-     $(this).tooltip('show')
-     $(this).addClass('fa-question-circle')
-     $(this).removeClass('fa-question')
-  })
-  .mouseleave(function(e) {
+    // Add explaination to read_after_post
+    if ($('#div_id_read_after_post').length == 1) {
+        $('#div_id_read_after_post').append(' <small><i class="fa questionMark fa-question " style="cursor:pointer;" data-toggle="tooltip" data-placement="right" title="In Read After Post mode a student cannot view other students\' posts until they have published a post of their own."></i></small>')
+    }
 
-     $(this).tooltip('hide')
-     $(this).removeClass('fa-question-circle')
-     $(this).addClass('fa-question')
-  });
+    // Add explaination to read_after_post
+    if ($('#div_id_is_active').length == 1) {
+        $('#div_id_is_active').append(' <small><i class="fa questionMark fa-question " style="cursor:pointer;" data-toggle="tooltip" data-placement="right" title="Students can participate in the activity only while it is active. On the other hand, students cannot access an inactive activity and it will not appear in their activity list. For example, deactivate an activity after an assignment deadline or while you are populating the activity content."></i></small>')
+    }
+
+    $('.questionMark').mouseenter(function (e) {
+
+        $(this).tooltip('show');
+        $(this).addClass('fa-question-circle');
+        $(this).removeClass('fa-question');
+    }).mouseleave(function (e) {
+
+        $(this).tooltip('hide');
+        $(this).removeClass('fa-question-circle');
+        $(this).addClass('fa-question');
+    });
 
 
 })
 
 
-function rteInit()
-    {
-        tinymce.init({
-            selector                    : "#InstructionTextArea",
-            inline                      : true,
-            schema                      : 'html5',
-            element_format              : 'html',
-            end_container_on_empty_block: true,
-            skin                        : 'flat_design_tinymce',
+function rteInit() {
+    tinymce.init({
+        selector: "#InstructionTextArea",
+        inline: true,
+        schema: 'html5',
+        element_format: 'html',
+        end_container_on_empty_block: true,
+        skin: 'flat_design_tinymce',
 
-            style_formats: [
-            {title: 'Headers', items: [
-                {title: 'Header 1', format: 'h1'},
-                {title: 'Header 2', format: 'h2'},
-                {title: 'Header 3', format: 'h3'},
-                {title: 'Header 4', format: 'h4'},
-                {title: 'Header 5', format: 'h5'},
-                {title: 'Header 6', format: 'h6'}
-            ]},
+        style_formats: [{
+            title: 'Headers',
+            items: [{
+                title: 'Header 1',
+                format: 'h1'
+            },
+            {
+                title: 'Header 2',
+                format: 'h2'
+            },
+            {
+                title: 'Header 3',
+                format: 'h3'
+            },
+            {
+                title: 'Header 4',
+                format: 'h4'
+            },
+            {
+                title: 'Header 5',
+                format: 'h5'
+            },
+            {
+                title: 'Header 6',
+                format: 'h6'
+            }
+            ]
+        },
 
-            {title: 'Font Sizes', items: [
-                {title: 'Tiny text', inline: 'span', styles: {fontSize: '10px'}},
-                {title: 'Small text', inline: 'span', styles: {fontSize: '13px'}},
-                {title: 'Normal text', inline: 'span', styles: {fontSize: '15px'}},
-                {title: 'Big text', inline: 'span', styles: {fontSize: '18px'}},
-                {title: 'Huge text', inline: 'span', styles: {fontSize: '22px'}}
-            ]},
+        {
+            title: 'Font Sizes',
+            items: [{
+                title: 'Tiny text',
+                inline: 'span',
+                styles: {
+                    fontSize: '10px'
+                }
+            },
+            {
+                title: 'Small text',
+                inline: 'span',
+                styles: {
+                    fontSize: '13px'
+                }
+            },
+            {
+                title: 'Normal text',
+                inline: 'span',
+                styles: {
+                    fontSize: '15px'
+                }
+            },
+            {
+                title: 'Big text',
+                inline: 'span',
+                styles: {
+                    fontSize: '18px'
+                }
+            },
+            {
+                title: 'Huge text',
+                inline: 'span',
+                styles: {
+                    fontSize: '22px'
+                }
+            }
+            ]
+        },
 
-            {title: 'Inline', items: [
-                {title: 'Bold', icon: 'bold', format: 'bold'},
-                {title: 'Italic', icon: 'italic', format: 'italic'},
-                {title: 'Underline', icon: 'underline', format: 'underline'},
-                {title: 'Strikethrough', icon: 'strikethrough', format: 'strikethrough'},
-                {title: 'Superscript', icon: 'superscript', format: 'superscript'},
-                {title: 'Subscript', icon: 'subscript', format: 'subscript'},
-                {title: 'Code', icon: 'code', format: 'code'}
-            ]},
+        {
+            title: 'Inline',
+            items: [{
+                title: 'Bold',
+                icon: 'bold',
+                format: 'bold'
+            },
+            {
+                title: 'Italic',
+                icon: 'italic',
+                format: 'italic'
+            },
+            {
+                title: 'Underline',
+                icon: 'underline',
+                format: 'underline'
+            },
+            {
+                title: 'Strikethrough',
+                icon: 'strikethrough',
+                format: 'strikethrough'
+            },
+            {
+                title: 'Superscript',
+                icon: 'superscript',
+                format: 'superscript'
+            },
+            {
+                title: 'Subscript',
+                icon: 'subscript',
+                format: 'subscript'
+            },
+            {
+                title: 'Code',
+                icon: 'code',
+                format: 'code'
+            }
+            ]
+        },
 
-            {title: 'Blocks', items: [
-                {title: 'Paragraph', format: 'p'},
-                {title: 'Blockquote', format: 'blockquote'},
-                {title: 'Div', format: 'div'},
-                {title: 'Pre', format: 'pre'}
-            ]},
+        {
+            title: 'Blocks',
+            items: [{
+                title: 'Paragraph',
+                format: 'p'
+            },
+            {
+                title: 'Blockquote',
+                format: 'blockquote'
+            },
+            {
+                title: 'Div',
+                format: 'div'
+            },
+            {
+                title: 'Pre',
+                format: 'pre'
+            }
+            ]
+        },
 
-            {title: 'Alignment', items: [
-                {title: 'Left', icon: 'alignleft', format: 'alignleft'},
-                {title: 'Center', icon: 'aligncenter', format: 'aligncenter'},
-                {title: 'Right', icon: 'alignright', format: 'alignright'},
-                {title: 'Justify', icon: 'alignjustify', format: 'alignjustify'}
-            ]}
+        {
+            title: 'Alignment',
+            items: [{
+                title: 'Left',
+                icon: 'alignleft',
+                format: 'alignleft'
+            },
+            {
+                title: 'Center',
+                icon: 'aligncenter',
+                format: 'aligncenter'
+            },
+            {
+                title: 'Right',
+                icon: 'alignright',
+                format: 'alignright'
+            },
+            {
+                title: 'Justify',
+                icon: 'alignjustify',
+                format: 'alignjustify'
+            }
+            ]
+        }
         ],
-            plugins          : [ "link image emoticons textcolor" ],
-            toolbar1         : "insertfile undo redo | styleselect | forecolor backcolor emoticons | link image",
-            image_advtab     : true,
-            forced_root_block: false,
-            entity_encoding  : 'raw',
-            menubar          : false
-        }); 
+        plugins: ['link image emoticons textcolor'],
+        toolbar1: 'insertfile undo redo | styleselect | forecolor backcolor emoticons | link image',
+        image_advtab: true,
+        forced_root_block: false,
+        entity_encoding: 'raw',
+        menubar: false
+    });
 }
-
-
-
--- static/js/base.js
+++ static/js/base.js
@@ -1,37 +1,39 @@
-$(function(){
-  // subscribe to a new course 
-  var subscribeURL = $("#courseSubscribeButton").data('subscribeurl');
-  $("#courseSubscribeButton").click(function(event){
-      event.preventDefault();
-      var GOTOURL              = subscribeURL.replace('ToReplace',$('#accesscodearea').val() );
-          window.location.href = GOTOURL;
-  });
+$(function () {
+    // subscribe to a new course 
+    var subscribeURL = $('#courseSubscribeButton').data('subscribeurl');
+    $('#courseSubscribeButton').click(function (event) {
+        event.preventDefault();
+        var GOTOURL = subscribeURL.replace('ToReplace', $('#accesscodearea').val());
+        window.location.href = GOTOURL;
+    });
 
-  // search table content
-  $(".search").keyup(function () {
-    var searchTerm  = $(".search").val();
-    var listItem    = $('.results tbody').children('tr');
-    var searchSplit = searchTerm.replace(/ /g, "'):containsi('");
-    
-  $.extend($.expr[':'], {'containsi': function(elem, i, match, array){
-        return (elem.textContent || elem.innerText || '').toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
-    }
-  });
-    
-  $(".results tbody tr").not(":containsi('" + searchSplit + "')").each(function(e){
-    $(this).attr('visible','false');
-  });
+    // search table content
+    $('.search').keyup(function () {
+        var searchTerm = $('.search').val();
+        var listItem = $('.results tbody').children('tr');
+        var searchSplit = searchTerm.replace(/ /g, '"):containsi("');
 
-  $(".results tbody tr:containsi('" + searchSplit + "')").each(function(e){
-    $(this).attr('visible','true');
-  });
+        $.extend($.expr[':'], {
+            'containsi': function (elem, i, match, array) {
+                return (elem.textContent || elem.innerText || '').toLowerCase().indexOf((match[3] || '').toLowerCase()) >= 0;
+            }
+        });
 
-  var jobCount = $('.results tbody tr[visible="true"]').length;
-    $('.counter').text(jobCount + ' item');
+        $('.results tbody tr').not('":containsi("' + searchSplit + '")').each(function (e) {
+            $(this).attr('visible', 'false');
+        });
 
-  if(jobCount == '0') {$('.no-result').show();}
-    else {$('.no-result').hide();}
-          });
+        $('.results tbody tr:containsi("' + searchSplit + '")').each(function (e) {
+            $(this).attr('visible', 'true');
+        });
 
+        var jobCount = $('.results tbody tr[visible="true"]').length;
+        $('.counter').text(jobCount + ' item');
+
+        if (jobCount == '0') {
+            $('.no-result').show();
+        } else {
+            $('.no-result').hide();
+        }
+    });
 });
- 
